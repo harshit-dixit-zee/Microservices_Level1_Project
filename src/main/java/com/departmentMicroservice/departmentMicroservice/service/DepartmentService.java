06f9dc82cbbe6c3064987169b437c9112a9025dc
harshit.dixit <harshit.dixit@zee.com>
2022-12-07 15:41:56 +0530

    userService

---
+++ src/main/java/com/departmentMicroservice/departmentMicroservice/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package com.departmentMicroservice.departmentMicroservice.service;
+
+import com.departmentMicroservice.departmentMicroservice.entity.Department;
+import com.departmentMicroservice.departmentMicroservice.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+    public Department saveDepartment(Department department) {
+        log.info("Inside service and saving to repository");
+        return departmentRepository.save(department);
+    }
+
+    public Department findById(Long departmentId) {
+
+        return departmentRepository.findByDepartmentId(departmentId);
+
+    }
+}
