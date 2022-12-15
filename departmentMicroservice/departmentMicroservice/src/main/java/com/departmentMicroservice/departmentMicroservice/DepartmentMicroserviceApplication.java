1d1b82dc28638d128843c74dee926c3d0c1f9e10
harshit-dixit-zee <110526238+harshit-dixit-zee@users.noreply.github.com>
2022-12-15 18:31:11 +0530

    Add files via upload

---
+++ departmentMicroservice/departmentMicroservice/src/main/java/com/departmentMicroservice/departmentMicroservice/DepartmentMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.departmentMicroservice.departmentMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentMicroserviceApplication.class, args);
+	}
+
+}
